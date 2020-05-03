c11e83df3ecac3a8890e5b79a30fbda48fd87156
kurobaneLia <353709464@qq.com>
2020-05-04 01:17:12 +0800

    添加controller全局跨域设置

--- src/main/java/com/game/wegame/controller/HelloController.java
+++ src/main/java/com/game/wegame/controller/HelloController.java
@@ -3,20 +3,26 @@ package com.game.wegame.controller;
 
 import com.game.wegame.entity.User;
 import com.game.wegame.service.interf.UserMapper;
+import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.nio.file.attribute.UserPrincipal;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin(origins = {"http://localhost:8888"})
 @Controller
 public class HelloController {
 
     /**
-     * @author Kurobane
+     * @author Kurobane Lia
      * @date 2020-4-27
      * spring ioc 依赖注入
      */
@@ -34,7 +40,7 @@ public class HelloController {
 
     @RequestMapping("/index2")
     public String index2(){
-        return "index";
+        return "manage";
     }
 
 /*    @RequestMapping("/index3")
@@ -75,5 +81,24 @@ public class HelloController {
     public List<User> selectAll() {
         List<User> list = userMapper.selectAll();
         return list;
+    }
+
+    /**
+     * 新增
+     * @return
+     */
+    @RequestMapping("/insert")
+    @ResponseBody
+    @Transactional  //该注解用于事务的控制
+    public int insert(HttpServletRequest request, HttpServletResponse response) {
+        String formData = request.getParameter("formData");
+        JSONObject formObject = JSONObject.fromObject(formData);
+        User user = new User();
+        user.setUsername((String)formObject.get("username"));
+        user.setPassword((String)formObject.get("password"));
+        int i = userMapper.insert(user);
+        int j=1/0;//用算术异常终止程序，由于开启了事务，已经执行insert操作的数据会回滚
+        System.out.println(1);
+        return i;
     }
 }
